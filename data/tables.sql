d8cf6b8723f9fb5cb133ab1e4097857719ed2a7a
s <diego.hilderink@iesdonana.org>
2020-01-27 19:44:55 +0100

    Restaurando cosas

--- data/tables.sql
+++
@@ -1,9 +0,0 @@
-
-DROP TABLE IF EXISTS usuarios CASCADE;
-
-CREATE TABLE usuarios
-(
-    id              bigserial    PRIMARY KEY
-  , nombre          varchar(255) NOT NULL
-  , passw	    varchar(255) NOT NULL
-);
--- scripts/empresa.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-psql -h localhost -U usuario -d datos < data/tables.sql
